5cb3aaf1bb8e58894f0e47e35ae0ab98a00ad920
Rajnish Mishra <37776460+RAJNISHMSHRA@users.noreply.github.com>
2020-06-01 14:41:30 +0530

    Updates

---
+++ precache-manifest.98f9df6c77914aff65795f4c0c837234.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d04e28a4c5cbbb6cd86aac3376db45",
+    "url": "/itunes-app/index.html"
+  },
+  {
+    "revision": "2d93845014c521dd8135",
+    "url": "/itunes-app/static/css/2.fb20f2ef.chunk.css"
+  },
+  {
+    "revision": "7ec44b813dbfddd1ea88",
+    "url": "/itunes-app/static/css/main.cb256519.chunk.css"
+  },
+  {
+    "revision": "2d93845014c521dd8135",
+    "url": "/itunes-app/static/js/2.edc0e986.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/itunes-app/static/js/2.edc0e986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ec44b813dbfddd1ea88",
+    "url": "/itunes-app/static/js/main.a3b3138e.chunk.js"
+  },
+  {
+    "revision": "f3155f1cfb15b607ddd9",
+    "url": "/itunes-app/static/js/runtime-main.4fc1fd22.js"
+  },
+  {
+    "revision": "cbbfe8b0ca2547fafd7f55d52e301d84",
+    "url": "/itunes-app/static/media/am.cbbfe8b0.png"
+  },
+  {
+    "revision": "d38ea9192414455b2a6725e5d1757c8b",
+    "url": "/itunes-app/static/media/animationmusic.d38ea919.gif"
+  },
+  {
+    "revision": "39762912dfe5da256d1b6c10fb080e5e",
+    "url": "/itunes-app/static/media/avator.39762912.jpg"
+  },
+  {
+    "revision": "edd64b4bd628bc6d52c757bfe394504f",
+    "url": "/itunes-app/static/media/not found.edd64b4b.gif"
+  }
+]);
